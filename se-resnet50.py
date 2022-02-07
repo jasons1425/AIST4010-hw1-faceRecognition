8d921021fcdda43daa6bdcf4a4f6019c086d4854
jasons1425 <jasonns1425@gmail.com>
2022-02-07 22:19:31 +0800

    Add se-resnet50 summary for different train and val crop size

--- se-resnet50.py
+++ se-resnet50.py
@@ -1,7 +1,7 @@
 from model.se_resnet import se_resnet50
 from data.augment import augmentation_train, augmentation_test
 from data.data import get_ds, get_loader
-from model.ft_helper import train_model, config_optim
+from model.ft_helper import train_model, config_optim, fivecrop_forward
 import torch.cuda as cuda
 import torch.optim as optim
 import torch.nn as nn
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     # data preparation
     BATCH_SIZE = 32
     IMG_RESIZE = 224
-    CROP_SIZE = 48
-    train_ds = get_ds('train', transformation=augmentation_train(CROP_SIZE, IMG_RESIZE, preprocess=False))
+    TRAIN_CROP_SIZE, VAL_CROP_SIZE = 48, 64
+    train_ds = get_ds('train', transformation=augmentation_train(TRAIN_CROP_SIZE, IMG_RESIZE, preprocess=False))
     train_loader = get_loader(train_ds, BATCH_SIZE, shuffle=True)
-    val_ds = get_ds('val', transformation=augmentation_test(CROP_SIZE, IMG_RESIZE, preprocess=False))
+    val_ds = get_ds('val', transformation=augmentation_test(VAL_CROP_SIZE, IMG_RESIZE, preprocess=False))
     val_loader = get_loader(val_ds, BATCH_SIZE, shuffle=True)
     dataloaders = {'train':  train_loader, 'val': val_loader}
 
@@ -22,21 +22,22 @@ if __name__ == "__main__":
     NUM_OF_CLASSES = 1000
     model = se_resnet50(NUM_OF_CLASSES, pretrained=False)
     device = 'cuda' if cuda.is_available() else 'cpu'
-    model = model.to(device)
+    model = model.half().to(device)
 
     # training settings
     criterion = nn.CrossEntropyLoss()
     optimizer = config_optim(optim.SGD, model_ft=model,  feature_extract=False,
                              lr=0.001, momentum=0.9, weight_decay=0.01)
-
     scheduler = optim.lr_scheduler.StepLR(optimizer, 10, gamma=0.1)
+    val_func = lambda inputs, net: fivecrop_forward(inputs, net, weights_ratio=[0.2, 0.2, 0.2, 0.2, 0.2])
+
 
     # train the model
     epochs = 10
     cuda.empty_cache()
     model_ft, hist = train_model(model, dataloaders, criterion,
-                                 optimizer, scheduler,
-                                 num_epochs=epochs, is_inception=False)
+                                 optimizer, scheduler, val_func=val_func,
+                                 num_epochs=epochs, is_inception=False, half=True)
     torch.save(model_ft.state_dict(), f'se-resnet50.pth')
 
 
