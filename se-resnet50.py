e56bbe3802e9bc6dba4dcb618cff9609c3113afe
jasons1425 <jasonns1425@gmail.com>
2022-02-08 10:52:29 +0800

    Add se-resnet50 summaries for 48-cropsize without augmentation

--- se-resnet50.py
+++ se-resnet50.py
@@ -11,10 +11,12 @@ if __name__ == "__main__":
     # data preparation
     BATCH_SIZE = 32
     IMG_RESIZE = 224
-    TRAIN_CROP_SIZE, VAL_CROP_SIZE = 48, 64
-    train_ds = get_ds('train', transformation=augmentation_train(TRAIN_CROP_SIZE, IMG_RESIZE, preprocess=False))
+    TRAIN_CROP_SIZE, VAL_CROP_SIZE = 48, 48
+    train_ds = get_ds('train', transformation=augmentation_train(TRAIN_CROP_SIZE, IMG_RESIZE,
+                                                                 preprocess=False, no_augment=True))
     train_loader = get_loader(train_ds, BATCH_SIZE, shuffle=True)
-    val_ds = get_ds('val', transformation=augmentation_test(VAL_CROP_SIZE, IMG_RESIZE, preprocess=False))
+    val_ds = get_ds('val', transformation=augmentation_test(VAL_CROP_SIZE, IMG_RESIZE,
+                                                            preprocess=False, no_augment=True))
     val_loader = get_loader(val_ds, BATCH_SIZE, shuffle=True)
     dataloaders = {'train':  train_loader, 'val': val_loader}
 
@@ -28,15 +30,15 @@ if __name__ == "__main__":
     criterion = nn.CrossEntropyLoss()
     optimizer = config_optim(optim.SGD, model_ft=model,  feature_extract=False,
                              lr=0.001, momentum=0.9, weight_decay=0.01)
-    scheduler = optim.lr_scheduler.StepLR(optimizer, 10, gamma=0.1)
-    val_func = lambda inputs, net: fivecrop_forward(inputs, net, weights_ratio=[0.2, 0.2, 0.2, 0.2, 0.2])
+    scheduler = optim.lr_scheduler.StepLR(optimizer, 10, gamma=0.9)
+    val_func = lambda inputs, net: fivecrop_forward(inputs, net)
 
 
     # train the model
     epochs = 10
     cuda.empty_cache()
     model_ft, hist = train_model(model, dataloaders, criterion,
-                                 optimizer, scheduler, val_func=val_func,
+                                 optimizer, scheduler, val_func=None,
                                  num_epochs=epochs, is_inception=False, half=True)
     torch.save(model_ft.state_dict(), f'se-resnet50.pth')
 
