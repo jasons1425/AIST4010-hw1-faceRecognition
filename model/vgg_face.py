ee0b6c54b8cdcc5e3f50cca28566ed1936fef27b
jasons1425 <jasonns1425@gmail.com>
2022-02-11 12:44:35 +0800

    Set up VGGFaceNet with residual blocks

--- model/vgg_face.py
+++ model/vgg_face.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-from model.vgg_face_module import VGGBlock
+from model.vgg_face_module import VGGBlock, VGGResBlock
 
 
 class VGGFaceNet(nn.Module):
@@ -8,11 +8,41 @@ class VGGFaceNet(nn.Module):
                  init=nn.init.kaiming_uniform_, bn=True):
         super(VGGFaceNet, self).__init__()
         self.pool = nn.MaxPool2d(2)
-        self.conv0 = VGGBlock(3, 64, 3, blocks[0], init=init, bn=bn)
+        self.conv0 = VGGBlock(input_channel, 64, 3, blocks[0], init=init, bn=bn)
         self.conv1 = VGGBlock(64, 128, 3, blocks[1], init=init, bn=bn)
         self.conv2 = VGGBlock(128, 256, 3, blocks[2], init=init, bn=bn)
         self.conv3 = VGGBlock(256, 512, 3, blocks[3], init=init, bn=bn)
         self.conv4 = VGGBlock(512, 512, 3, blocks[4], init=init, bn=bn)
+        self.fc_conv1 = nn.Conv2d(512, 4096, 7)
+        init(self.fc_conv1.weight)
+        self.fc_conv2 = nn.Conv2d(4096, 4096, 1)
+        init(self.fc_conv1.weight)
+        self.fc_conv3 = nn.Conv2d(4096, num_of_classes, 1)
+        self.flatten = nn.Flatten()
+        self.dropout = nn.Dropout2d(dropout)
+
+    def forward(self, x):
+        output = x
+        for block in [self.conv0, self.conv1, self.conv2, self.conv3, self.conv4]:
+            output = self.pool(block(output))
+
+        output = self.dropout(self.fc_conv1(output))
+        output = self.dropout(self.fc_conv2(output))
+        output = self.fc_conv3(output)
+        return self.flatten(output)
+
+
+class VGGFaceResNet(nn.Module):
+    def __init__(self, input_channel=3, num_of_classes=1000,
+                 blocks=[2, 2, 3, 3, 3], dropout=0.5,
+                 init=nn.init.kaiming_uniform_, bn=True):
+        super(VGGFaceResNet, self).__init__()
+        self.pool = nn.MaxPool2d(2)
+        self.conv0 = VGGResBlock(input_channel, 64, 3, blocks[0], init=init, bn=bn)
+        self.conv1 = VGGResBlock(64, 128, 3, blocks[1], init=init, bn=bn)
+        self.conv2 = VGGResBlock(128, 256, 3, blocks[2], init=init, bn=bn)
+        self.conv3 = VGGResBlock(256, 512, 3, blocks[3], init=init, bn=bn)
+        self.conv4 = VGGResBlock(512, 512, 3, blocks[4], init=init, bn=bn)
         self.fc_conv1 = nn.Conv2d(512, 4096, 7)
         init(self.fc_conv1.weight)
         self.fc_conv2 = nn.Conv2d(4096, 4096, 1)
--- model/vgg_face_module.py
+++ model/vgg_face_module.py
@@ -21,3 +21,43 @@ class VGGBlock(nn.Module):
 
     def forward(self, x):
         return self.conv_blocks(x)
+
+
+class VGGResLayer(nn.Module):
+    def __init__(self, in_channel, out_channel, ksize,
+                 init=nn.init.kaiming_uniform_, bn=True):
+        super(VGGResLayer, self).__init__()
+
+        conv_layer = nn.Conv2d(in_channel, out_channel, ksize, padding=ksize // 2)
+        if init:
+            init(conv_layer.weight)
+        conv_stack = [conv_layer]
+        if bn:
+            conv_stack.append(nn.BatchNorm2d(out_channel))
+
+        self.res = (in_channel == out_channel)
+        self.conv_stack = nn.Sequential(*conv_stack)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        output = self.conv_stack(x)
+        if self.res:
+            output += x
+        return self.relu(output)
+
+
+class VGGResBlock(nn.Module):
+    def __init__(self, in_channel, out_channel, ksize, blocks,
+                 init=nn.init.kaiming_uniform_, bn=True):
+        super(VGGResBlock, self).__init__()
+        conv_blocks = []
+        for _ in range(blocks):
+            if in_channel:
+                conv_blocks.append(VGGResLayer(in_channel, out_channel, ksize, init=init, bn=bn))
+                in_channel = 0  # disable in_channel
+            else:
+                conv_blocks.append(VGGResLayer(out_channel, out_channel, ksize, init=init, bn=bn))
+        self.conv_blocks = nn.Sequential(*conv_blocks)
+
+    def forward(self, x):
+        return self.conv_blocks(x)
