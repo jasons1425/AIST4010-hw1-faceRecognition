3e2443861239a307cfca9be13ce1e3792a45a800
jasons1425 <jasonns1425@gmail.com>
2022-02-13 16:24:18 +0800

    Add vggface-resnet summary for extra 5 epoch on 7635 with constant lr=1e-5

--- vggface-resnet.py
+++ vggface-resnet.py
@@ -24,22 +24,22 @@ if __name__ == "__main__":
     NUM_OF_CLASSES = 1000
     model = VGGFaceResNet(3, NUM_OF_CLASSES)
     device = 'cuda' if cuda.is_available() else 'cpu'
-    model.load_state_dict(torch.load(r"trials/vggface-resnet-4900.pth"))
+    model.load_state_dict(torch.load(r"trials/vggface-resnet-7635.pth"))
     model = model.half().to(device)
 
     # training settings
     criterion = nn.CrossEntropyLoss()
     optimizer = config_optim(optim.SGD, model_ft=model,  feature_extract=False,
-                             lr=0.001, momentum=0.9, weight_decay=0.01)
-    scheduler_lambda_func = lambda epoch: 1 if epoch < 5 else (0.1 if epoch < 25 else 0.01)
+                             lr=0.00001, momentum=0.9, weight_decay=0.01)
+    # scheduler_lambda_func = lambda epoch: 1 if epoch < 5 else (0.1 if epoch < 25 else 0.01)
     # scheduler = optim.lr_scheduler.StepLR(optimizer, 10, gamma=0.1)
-    scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=scheduler_lambda_func)
+    # scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=scheduler_lambda_func)
     val_func = lambda inputs, net: fivecrop_forward(inputs, net)
 
     # train the model
-    epochs = 30
+    epochs = 5
     cuda.empty_cache()
     model_ft, hist = train_model(model, dataloaders, criterion,
-                                 optimizer, scheduler=scheduler, val_func=val_func,
+                                 optimizer, scheduler=None, val_func=val_func,
                                  num_epochs=epochs, is_inception=False, half=True)
     torch.save(model_ft.state_dict(), f'vggface-resnet.pth')
