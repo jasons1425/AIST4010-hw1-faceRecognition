1136dea50697265bd879035859f542305c0eeaf8
jasons1425 <jasonns1425@gmail.com>
2022-02-18 22:32:29 +0800

    Add vggface resnet summary for extra 40 epochs on 5635 with varying lr

--- vggface-resnet.py
+++ vggface-resnet.py
@@ -31,16 +31,16 @@ if __name__ == "__main__":
     criterion = nn.CrossEntropyLoss()
     optimizer = config_optim(optim.SGD, model_ft=model,  feature_extract=False,
                              lr=0.001, momentum=0.9, weight_decay=0.001)
-    # scheduler_lambda_func = lambda epoch: 1 if epoch < 5 else (0.1 if epoch < 25 else 0.01)
+    scheduler_lambda_func = lambda epoch: 1 if epoch < 5 else (0.1 if epoch < 35 else 0.01)
     # scheduler_lambda_func = lambda epoch: 1 if epoch < 10 else 0.1
     # scheduler = optim.lr_scheduler.StepLR(optimizer, 10, gamma=0.1)
-    # scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=scheduler_lambda_func)
+    scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=scheduler_lambda_func)
     val_func = lambda inputs, net: fivecrop_forward(inputs, net)
 
     # train the model
-    epochs = 20
+    epochs = 40
     cuda.empty_cache()
     model_ft, hist = train_model(model, dataloaders, criterion,
-                                 optimizer, scheduler=None, val_func=val_func,
+                                 optimizer, scheduler=scheduler, val_func=val_func,
                                  num_epochs=epochs, is_inception=False, half=True)
     torch.save(model_ft.state_dict(), f'vggface-resnet.pth')
