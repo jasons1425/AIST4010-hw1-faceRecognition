e0e39f2539f958eef31fb52f81bc6238ef722391
jasons1425 <jasonns1425@gmail.com>
2022-02-20 12:04:51 +0800

    Add vggface-resnet summary for extra 25 epochs on 7505 with lr=1e-4

--- vggface-resnet.py
+++ vggface-resnet.py
@@ -26,13 +26,13 @@ if __name__ == "__main__":
     NUM_OF_CLASSES = 1000
     model = VGGFaceResNet(3, NUM_OF_CLASSES)
     device = 'cuda' if cuda.is_available() else 'cpu'
-    model.load_state_dict(torch.load(r"trials/vggface-resnet-7635.pth"))
+    model.load_state_dict(torch.load(r"trials/vggface-resnet-7505.pth"))
     model = model.half().to(device)
 
     # training settings
     criterion = nn.CrossEntropyLoss()
     optimizer = config_optim(optim.SGD, model_ft=model,  feature_extract=False,
-                             lr=1e-5, momentum=0.9, weight_decay=0.01)
+                             lr=1e-4, momentum=0.9, weight_decay=0.01)
     # scheduler_lambda_func = lambda epoch: 1 if epoch < 5 else (0.1 if epoch < 35 else 0.01)
     # scheduler_lambda_func = lambda epoch: 1 if epoch < 10 else 0.1
     # scheduler = optim.lr_scheduler.StepLR(optimizer, 10, gamma=0.1)
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     val_func = lambda inputs, net: fivecrop_forward(inputs, net)
 
     # train the model
-    epochs = 10
+    epochs = 25
     cuda.empty_cache()
     model_ft, hist = train_model(model, dataloaders, criterion,
                                  optimizer, scheduler=None, val_func=val_func,
